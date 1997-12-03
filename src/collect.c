cdb6e1cbf43e9d7bfed3b05aed0928ae3e568975
Werner Nickel <nickel@mathematik.tu-darmstadt.de>
1997-12-03 06:22:13 +0000

    Changed Error() to a function that returns an error code.

--- src/collect.c
+++ src/collect.c
@@ -15,7 +15,9 @@ gen     g;
 
 {	printf( "Error in Collect() while treating generator %d:\n", g );
         printf( "      %s\n", str );
-	exit( 7 );
+/*	exit( 7 );*/
+	
+	return 7;
 }
 
 /*
@@ -35,7 +37,7 @@ gen     g;
 **    STACKHEIGHT.
 */
 
-#define STACKHEIGHT	(1 << 10)
+#define STACKHEIGHT	(1 << 16)
 
 static	word	WordStack[STACKHEIGHT];
 static	exp	WordExpStack[STACKHEIGHT];
@@ -66,7 +68,7 @@ exp	e;
         if( (g = gs[ sp ]->g) != EOW ) {
             ag = abs( g );
             if( g < 0 && Exponent[-g] != 0 ) 
-                Error( "Inverse of a generator with power relation", ag );
+                return Error( "Inverse of a generator with power relation", ag );
             e = (ag == Commute[ag]) ? gs[ sp ]->e : 1;
             if( (ges[ sp ] -= e) == 0 ) {
                 /* The power of the generator g will have been moved
@@ -80,7 +82,7 @@ exp	e;
             for( h = Commute[ag]; h > ag; h-- )
                 if( lhs[h] != 0 ) {
                     if( ++sp == STACKHEIGHT )
-                        Error( "Out of stack space", ag );
+                        return Error( "Out of stack space", ag );
                     if( lhs[ h ] > 0 ) {
                         gs[ sp ]  = ws[ sp ] = C[ h ][ g ];
                         wes[ sp ] = lhs[h]; lhs[ h ] = 0;
@@ -94,19 +96,19 @@ exp	e;
                 }
             lhs[ ag ] += e * sgn(g);
             if ( ((lhs[ag] << 1) >> 1) != lhs[ag] )
-                Error( "Possible integer overflow", ag );
+                return Error( "Possible integer overflow", ag );
             if( Exponent[ag] != 0 )
                 while( lhs[ag] >= Exponent[ag] ) {
                     if( (rhs = P[ ag ]) != (word)0 ) {
                         if( ++sp == STACKHEIGHT )
-                            Error( "Out of stack space", ag );
+                            return Error( "Out of stack space", ag );
                         gs[ sp ] = ws[ sp ] = rhs;
                         wes[ sp ] = 1;
                         ges[ sp ] = gs[ sp ]->e;
                     }
                     lhs[ ag ] -= Exponent[ ag ];
                     if ( ((lhs[ag] << 1) >> 1) != lhs[ag] )
-                        Error( "Possible integer overflow", ag );
+                        return Error( "Possible integer overflow", ag );
                 }
         }
         else {
@@ -153,7 +155,11 @@ word	u, v;
 	    exit( 2 );
 	}
 
-	Collect( uvec, u, 1 );
+	if( Collect( uvec, u, 1 ) ) {
+	    Free( x );
+	    Free( uvec );
+	    return (word)0;
+	}
 
 	for( lv = lx = 0, g = 1; g <= NrPcGens+NrCenGens; g++ ) {
 	    if( v[lv].g == g )       ev =  v[ lv++ ].e;
@@ -265,7 +271,53 @@ int	n;
 	return u;
 }
 
-word	Commutator( v, w )
+word    Commutator( u, v )
+word    u, v;
+
+{
+    expvec u1, u2, v1, v2, x;
+    gpower y[2];
+    word w;
+    int i;
+
+    y[0].g = y[1].g = EOW;
+    y[0].e = y[1].e = 0;
+
+    u1 = ExpVecWord( u ); u2 = ExpVecWord( u );
+    v1 = ExpVecWord( v ); v2 = ExpVecWord( v );
+    x  = ExpVecWord( y );
+    for( i = 1; i <= NrPcGens+NrCenGens; i++ ) {
+        x[i] = u2[i]+v2[i] - (v1[i] + u1[i]);
+        if( Exponent[i] != 0 ) {
+            while( x[i] < 0 ) x[i] += Exponent[i];
+            if( x[i] >= Exponent[i] ) x[i] -= Exponent[i];
+        }
+        if(  x[i] != 0 ) {
+            if(  x[i] > 0 ) { y[0].g =  i; y[0].e =   x[i]; }
+            else            { y[0].g = -i; y[0].e =  -x[i]; }
+            if( Collect( u1, y, 1 ) ) { w = (word)0; goto exit; }
+        }
+        if( u1[i] != 0 ) {
+            if( u1[i] > 0 ) { y[0].g =  i; y[0].e =  u1[i]; }
+            else            { y[0].g = -i; y[0].e = -u1[i]; }
+            if( Collect( v1, y, 1 ) ) { w = (word)0; goto exit; }
+        }
+        if( v2[i] != 0 ) {
+            if( v2[i] > 0 ) { y[0].g =  i; y[0].e =  v2[i]; }
+            else            { y[0].g = -i; y[0].e = -v2[i]; }
+            if( Collect( u2, y, 1 ) ) { w = (word)0; goto exit; }
+        }
+    }
+    
+    w = WordExpVec( x );
+
+exit:
+    Free( u1 ); Free( u2 ); Free( v1 ); Free( v2 ); Free( x );
+    
+    return w;
+}
+
+word	Commutator2( v, w )
 word	v, w;
 
 {	expvec	ev;
