8aacbe720c0d502ed8ed627052bb94264ebb827d
Werner Nickel <nickel@mathematik.tu-darmstadt.de>
1998-12-01 08:40:25 +0000

    Initial revision

---
+++ src/gap.c
@@ -0,0 +1,81 @@
+
+void	printWord( w )
+word	w;
+
+{	if( w == (word)0 || w->g == EOW ) {
+	    printf( "One(F)" );
+	    return;
+	}
+	
+	while( w->g != EOW ) {
+	    if( w->g > 0 ) {
+                printf( "F.%d", w->g );
+		if( w->e != 1 )
+		    printf( "^%d", w->e );
+	    }
+	    else {
+                printf( "F.%d", w->g );
+		printf( "^%d", -w->e );
+	    }
+	    w++;
+	    if( w->g != EOW ) putchar( '*' );
+	}
+}
+
+
+void	PrintGapPcPres() {
+
+	gen	g;
+	long	i, j, first = 1;
+
+        /*
+        **  Commands that create the appropriate free group and the
+        **  collector. 
+        */
+	printf( "F := FreeGroup( %d );\n", NrPcGens+NrCenGens );
+        printf( "dt := DeepThoughtCollector( F, [ " );
+	for( i = 1; i <= NrPcGens+NrCenGens; i++ )
+            printf( "%d, ", Exponent[i] );
+        printf( " ] );\n" );
+
+        /*
+        **  Set the power relations.
+        */
+	for( i = 1; i <= NrPcGens+NrCenGens; i++ )
+	    if( Exponent[i] != (exp)0 && 
+                Power[i] != (word)0 && Power[i]->g != EOW ) {
+                printf( "SetPower( dt, %d, ", i );
+                printGapWord( Power[i] );
+
+            }
+
+        /*
+        **  Set the conjugate relations.
+        */
+	for( j = 1; j <= NrPcGens; j++ ) {
+	    i = 1;
+	    while( i < j && Wt(i) + Wt(j) <= Class + (NrCenGens==0?0:1) ) {
+		/* print Conjugate[j][i] */
+                printf( "SetConjugate( dt, j, i, " );
+		printGapWord( Conjugate[j][i] );
+		if( Exponent[i] == (exp)0 ) {
+                    printf( "SetConjugate( dt, j, -i, " );
+		    printGapWord( Conjugate[j][-i] );
+		}
+		if( 0 && Exponent[j] == (exp)0 ) {
+                    printf( "SetConjugate( dt, -j, i, " );
+		    printGapWord( Conjugate[-j][i] );
+		}
+		if( 0 && Exponent[i] + Exponent[j] == (exp)0 ) {
+                    printf( "SetConjugate( dt, -j, -i, " );
+		    printGapWord( Conjugate[-j][-i], 'A' );
+		}
+		i++;
+	    }
+	}
+
+	printf( "\n#    Class : %d\n", Class );
+	printf( "#    Nr of generators of each class :" );
+	for( i = 1; i <= Class; i++ ) printf( " %d", Dimension[i] );
+	printf( "\n" );
+}
