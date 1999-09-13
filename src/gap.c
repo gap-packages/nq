1ab225e74d8fa9cb8f85e647ff4264da2c0b60eb
Werner Nickel <nickel@mathematik.tu-darmstadt.de>
1999-09-13 14:48:52 +0000

    A new output routine that prints a pc-presentation as an array of generator exponent lists.

--- src/gap.c
+++ src/gap.c
@@ -96,17 +96,17 @@ void	PrintGapPcPres() {
                 printf( "SetConjugate( NqCollector, %d, %d, ", j, i );
 		printGapWord( Conjugate[j][i] );
                 printf( " );\n" );
-		if( 1 && Exponent[i] == (exp)0 ) {
+		if( 0 && Exponent[i] == (exp)0 ) {
                     printf( "SetConjugate( NqCollector, %d, %d, ", j, -i );
 		    printGapWord( Conjugate[j][-i] );
                     printf( " );\n" );
 		}
-		if( 1 && Exponent[j] == (exp)0 ) {
+		if( 0 && Exponent[j] == (exp)0 ) {
                     printf( "SetConjugate( NqCollector, %d, %d, ", -j, i );
 		    printGapWord( Conjugate[-j][i] );
                     printf( " );\n" );
 		}
-		if( 1 && Exponent[i] + Exponent[j] == (exp)0 ) {
+		if( 0 && Exponent[i] + Exponent[j] == (exp)0 ) {
                     printf( "SetConjugate( NqCollector, %d, %d, ", -j, -i );
 		    printGapWord( Conjugate[-j][-i], 'A' );
                     printf( " );\n" );
@@ -129,4 +129,142 @@ void	PrintGapPcPres() {
 	printf( "NqRanks := [ " );
 	for( i = 1; i <= Class; i++ ) printf( " %d,", Dimension[i] );
 	printf( "];\n" );
+}
+
+void	printGapList( w )
+word	w;
+
+{       int nrc = 30;      /* something has already been printed */
+
+	if( w == (word)0 || w->g == EOW ) { printf( "[]" ); return; }
+	
+	printf( "[ " );
+	while( w->g != EOW ) {
+	    if( w->g > 0 ) {
+                nrc += printf( " %d,", w->g );
+#               ifdef LONGLONG
+                nrc += printf( " %Ld,", w->e );
+#               else
+		nrc += printf( " %d,", w->e );
+#               endif
+            }
+            else {
+                nrc += printf( " %d,", -w->g );
+#               ifdef LONGLONG
+                nrc += printf( " %Ld,", -w->e );
+#               else
+                nrc += printf( " %d,", -w->e );
+#               endif
+            }
+            w++;
+	    /* Avoid long lines, because GAP can't read them. */
+            if( w->g != EOW && nrc > 70 ) { printf( "\\\n  " ); nrc = 0; }
+        }
+	printf( " ]" );
+}
+
+void printConjugates ( conj )
+word **conj;
+
+{
+}
+
+void    PrintRawGapPcPres() {
+
+        gen     g;
+        long    i, j;
+	long    cl;
+
+        /*
+        **  Output the number of generators first and their relative
+        **  orders.  
+        */
+        printf( "NqNrGens :=  %d;\n", NrPcGens+NrCenGens );
+        printf( "NqRelOrders := [ " );
+        for( i = 1; i <= NrPcGens+NrCenGens; i++ ) {
+#               ifdef LONGLONG
+                    printf( "%Ld,", Exponent[i] );
+#               else
+                    printf( "%d,", Exponent[i] );
+#               endif
+        }
+        printf( " ];\n" );
+
+        /*
+        **  Print the power relations.
+        */
+        printf( "NqPowers := [];\n" );
+        for( i = 1; i <= NrPcGens+NrCenGens; i++ )
+            if( Exponent[i] != (exp)0 && 
+                Power[i] != (word)0 && Power[i]->g != EOW ) {
+                printf( "NqPower[ %d ] := ", i );
+                printGapList( Power[i] );
+                printf( ";\n" );
+            }
+
+        /*
+        **  Print the conjugate relations.
+        */
+	cl = Class + (NrCenGens==0 ? 0 : 1);
+	printf( "NqConj := [];\n" );
+        for( j = 1; j <= NrPcGens; j++ ) {
+	    printf( "NqConj[ %d ] := [];\n", j );
+            for( i = 1; i < j && Wt(i) + Wt(j) <= cl; i++ ) {
+                /* print Conjugate[j][i] */
+                printf( "NqConj[ %d ][ %d ] := ", j, i );
+                printGapList( Conjugate[j][i] );
+                printf( ";\n" );
+	    }
+	}
+
+	printf( "NqConjI := [];\n" );
+        for( j = 1; j <= NrPcGens; j++ ) {
+	    printf( "NqConjI[ %d ] := [];\n", j );
+            for( i = 1; i < j && Wt(i) + Wt(j) <= cl; i++ ) {
+	      if( Exponent[i] == (exp)0 ) {
+                printf( "NqConjI[ %d ][ %d ] := ", j, i );
+                printGapList( Conjugate[j][-i] );
+                printf( ";\n" );
+	      }
+	    }
+	}
+
+	printf( "NqIConj := [];\n" );
+        for( j = 1; j <= NrPcGens; j++ ) {
+	    printf( "NqIConj[ %d ] := [];\n", j );
+            for( i = 1; i < j && Wt(i) + Wt(j) <= cl; i++ ) {
+	      if( Exponent[j] == (exp)0 ) {
+                printf( "NqIConj[ %d ][ %d ] := ", j, i );
+                printGapList( Conjugate[-j][i] );
+                printf( ";\n" );
+	      }
+	    }
+	}
+
+	printf( "NqIConjI := [];\n" );
+        for( j = 1; j <= NrPcGens; j++ ) {
+	    printf( "NqIConjI[ %d ] := [];\n", j );
+            for( i = 1; i < j && Wt(i) + Wt(j) <= cl; i++ ) {
+	      if( Exponent[i] + Exponent[j] == (exp)0 ) {
+                printf( "NqIConjI[ %d ][ %d ] := ", j, i );
+                printGapList( Conjugate[-j][-i] );
+                printf( ";\n" );
+	      }
+	    }
+	}
+
+        /*
+        **  Print the epimorphism.  It is sufficient to list the images.
+        */
+        printf( "NqImages := [\n" );
+        for( i = 1; i <= NumberOfGens(); i++ ) {
+            printGapList( Epimorphism( i ) );
+            printf( ",\n" );
+        }
+        printf( "];\n" );
+
+        printf( "NqClass := %d;\n", Class );
+        printf( "NqRanks := [ " );
+        for( i = 1; i <= Class; i++ ) printf( " %d,", Dimension[i] );
+        printf( "];\n" );
 }
