fbce39637fcd0099826b025ca3f0fa3098eeedc5
Werner Nickel <nickel@mathematik.tu-darmstadt.de>
1999-09-13 16:43:36 +0000

    mem.h declares Allocate(), etc., and includes malloc.h.

---
+++ src/mem.h
@@ -0,0 +1,12 @@
+/****************************************************************************
+**
+**    mem.h                           NQ                       Werner Nickel
+**                                         Werner.Nickel@math.rwth-aachen.de
+*/
+
+#include <malloc.h>
+
+extern void	*Allocate();
+extern void	*ReAllocate();
+extern void	Free();
+
