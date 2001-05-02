d61f5bc229a899bd16527ce4a0ab6a201fdd5786
Werner Nickel <nickel@mathematik.tu-darmstadt.de>
2001-05-02 14:41:15 +0000

    New version of NQ.  These are the files dealing with enumerating the instances of an identity.					    WN

---
+++ src/instances.h
@@ -0,0 +1,10 @@
+/*****************************************************************************
+**
+**    instances.h                     NQ                       Werner Nickel
+**                                         Werner.Nickel@math.rwth-aachen.de
+*/
+
+
+extern word    *Instances;
+
+extern void    EvalIdenticalRelation();
